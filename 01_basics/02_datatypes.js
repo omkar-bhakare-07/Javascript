997564e6d5a05716cc7a581e21c72fe418a720e9
Omkar Bhakare <107770872+omkar-bhakare-07@users.noreply.github.com>
2023-10-04 21:24:04 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name  = "Omkar"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// number = range is 2 to power 53
+// bigint
+// string = "ghost is coming"
+// boolean = true/false
+// null
+// undefined
+// symbol = mostly used for uniqueness
+// object
+
+console.log(typeof 1, typeof true, typeof "omkar", typeof null, typeof undefined, typeof Symbol);
