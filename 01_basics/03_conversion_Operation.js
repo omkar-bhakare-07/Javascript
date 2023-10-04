2357eeb47b333c68bf9e3ae81499414c52978d48
Omkar Bhakare <107770872+omkar-bhakare-07@users.noreply.github.com>
2023-10-04 21:52:14 +0000

    conversion 1

---
+++ 01_basics/03_conversion_Operation.js
@@ -0,0 +1,53 @@
+let score = "35aaa"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log("----------------------");
+
+let bool = true
+console.log(typeof bool);
+let boolNumber = Number(bool)
+console.log(typeof boolNumber);
+console.log(boolNumber);
+console.log("----------------------");
+
+let string = "OMkar"
+console.log(typeof string);
+let stringNumber = Number(string)
+console.log(typeof boolNumber);
+console.log(stringNumber);
+
+console.log("----------------------");
+
+let UD = undefined
+console.log(typeof UD);
+let UDnumber = Number(UD)
+console.log(typeof UDnumber);
+console.log(UDnumber)
+console.log("----------------------");
+// 35 = 35
+// 35aaa =NaN
+// true = 1 & false = 0
+
+
+let number = "Omkar"
+let booleanNumber = Boolean(number)
+console.log(typeof number);
+console.log(booleanNumber);
+console.log("----------------------");
+// 1 = true;
+// "" = false;
+// 0 = false;
+// "Omkar" = true;
+
+
+let nums = 7
+let numstring = String(nums)
+console.log(typeof nums);
+console.log(numstring);
+console.log(typeof numstring);
